150e86b0308e555ce9daf7006e1c7fbcc86582e1
ForeytKL22 <krforeyt@gmail.com>
2021-09-19 14:31:01 -0600

    created html page

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
+const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
 
+const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
+const pageHTML = generatePage(name, github);
 
-const printProfileData = profileDataArr => {
-    // This....
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-    console.log('===============');
-
-    // Is the same as this....
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
-
-
-
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
