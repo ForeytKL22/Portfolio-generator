04123624840b280806a62ca6c28ad7be5f710613
ForeytKL22 <krforeyt@gmail.com>
2021-09-21 18:41:13 -0600

    added inquire and first question

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer 
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name'
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+  });
 
-console.log(profileDataArgs);
+// const fs = requre('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generagePage(name, github);
 
-console.log(name, github);
+// fs.writefile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const pageHTML = generatePage(name, github);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
